d4b359d737635f1224b603fd90a277518a7cee0f
Victor-creat <victorkihiko211@gmail.com>
2024-12-04 20:13:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 6.2;
+const num2 = 10;
+const multiply = num1 * num2;  // This should result in 60, so we need to adjust to make it 62.
 
+const random = Math.floor(Math.random() * 100) + 1;  // Generates a random integer greater than 0.
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;  // Calculates the remainder, which should be 4.
+
+const max = Math.max(1, 2, 3, 20);  // Returns the highest number in the set, which is 20.
 // Write your code here
